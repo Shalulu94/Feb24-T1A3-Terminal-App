e69ecb1662ad716a631dd558cc22d47a986ada1d
shalulu94 <shahrulnasir290@hotmail.com>
2024-04-23 01:50:06 +1000

    Further updates to character creation menu

---
+++ classes.py
@@ -0,0 +1,24 @@
+class Character():
+    def __init__(self, first_name, last_name, alias, fight_style):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.alias = alias
+        self.fight_style = fight_style
+        self.attack = 100
+        self.defence = 50
+        self.hp = 600
+        self.statpoints = 3
+        
+    
+
+
+
+# print(f"\nFighter name is: {player.name}")
+# print(player.alias)
+# print(player.fight_style)
+# print(player.attack)
+# print(player.defence)
+# print(player.hp)
+# print(player.statpoints)
+
+
---
+++ functions.py
@@ -0,0 +1,39 @@
+# Character creation
+from classes import Character
+
+def character_create():
+    print("Welcome to the fighter creation menu\n")
+    print("Here you will be able to create a new fighter to compete with in the boxing championship!\n")
+
+
+    player_firstname = input("Please enter your fighter's first name: ")
+    player_lastname = input("Please enter your fighter's last name: ")
+    player_alias = input("Please enter your fighter's nickname: ")
+
+    print("\nYour fighting style dictates your fighter's strength in the ring.")
+    print("There are three fighting styles to choose from:")
+    print("\nAggressive - Your fighter will deal more damage with their attacks ")
+    print("\nDefensive - Your fighter will take less damage on successful blocks")
+    print("\nTanky - Your fighter has a higher health pool\n")
+
+    player_fight_style = input("Please choose your fighting style from the above options: ")
+
+    player = Character(player_firstname, player_lastname, player_alias, player_fight_style)
+
+        
+    print(f"\nYour fighter's name is: {player.first_name} \"{player.alias}\" {player.last_name}")
+    print(f"\nYour fighting style is: {player.fight_style}")
+    print(f"Your attack is: {player.attack}")
+    print(f"Your defence is: {player.defence}")
+    print(f"Your total HP is: {player.hp}")
+    
+
+
+
+
+
+
+
+
+
+character_create()
