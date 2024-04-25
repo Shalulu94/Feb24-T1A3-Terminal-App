57452a8ef02d9c623eea99425d8c2c4b8c55e89e
Shalulu94 <shahrulnasir290@hotmail.com>
2024-04-25 15:45:33 +1000

    Added error capture on upgrades menu

--- upgrade_stats.py
+++ upgrade_stats.py
@@ -5,77 +5,84 @@ import os
 
 def upgrades():
    
-
-    while character_creation.player.statpoints > 0:
-        os.system('clear')
-
-        print("Welcome to the upgrade menu")
-        print("\nHere you will be able to spend available stat points to upgrade your fighter's stats")
-        print(f"you currently have {character_creation.player.statpoints} available to use")
-        print("\nPlease select from the following options:")
-        print("1. Allocate stat points")
-        print("2. View your fighter details")
-        print("3. Return to previous menu")
-
-        user_input = int(input("\nPlease input your selection between 1 and 3:\n"))
-
-        if user_input == 1:
-            
+    try:
+        while character_creation.player.statpoints > 0:
             os.system('clear')
 
-            print("Please choose which stat you would like to upgrade:\n")
-            print("[Attack] - Each additional point increases Attack by 25")
-            print("[Defence] - Each additional point increases Defence by 25")
-            print("[HP] - Each additional point increases HP by 150")
+            print("Welcome to the upgrade menu")
+            print("\nHere you will be able to spend available stat points to upgrade your fighter's stats")
+            print(f"you currently have {character_creation.player.statpoints} available to use")
+            print("\nPlease select from the following options:")
+            print("1. Allocate stat points")
+            print("2. View your fighter details")
+            print("3. Return to previous menu")
 
-            upgrade_input = input("\nPlease type in which stat you would like to upgrade: ")
-            upgrade_input_low = upgrade_input.lower()
+            user_input = int(input("\nPlease input your selection between 1 and 3:\n"))
 
-            if upgrade_input_low == "attack":
-                character_creation.player.attack += 25
-                character_creation.player.statpoints -= 1
-                print("\nAttack successfully upgraded!")
-                input("\nPress Enter to return")
+            if user_input == 1:
+                
+                os.system('clear')
 
-            elif upgrade_input_low == "defence":
-                character_creation.player.defence += 25
-                character_creation.player.statpoints -= 1
-                print("\nDefence successfully upgraded!")
-                input("\nPress Enter to return")
+                print("Please choose which stat you would like to upgrade:\n")
+                print("[Attack] - Each additional point increases Attack by 25")
+                print("[Defence] - Each additional point increases Defence by 25")
+                print("[HP] - Each additional point increases HP by 150")
 
-            elif upgrade_input_low == "hp":
-                character_creation.player.hp += 25
-                character_creation.player.statpoints -= 1
-                print("\nHP successfully upgraded!")
-                input("\nPress Enter to return")
+                upgrade_input = input("\nPlease type in which stat you would like to upgrade: ")
+                upgrade_input_low = upgrade_input.lower()
+
+                if upgrade_input_low == "attack":
+                    character_creation.player.attack += 25
+                    character_creation.player.statpoints -= 1
+                    print("\nAttack successfully upgraded!")
+                    input("\nPress Enter to return")
+
+                elif upgrade_input_low == "defence":
+                    character_creation.player.defence += 25
+                    character_creation.player.statpoints -= 1
+                    print("\nDefence successfully upgraded!")
+                    input("\nPress Enter to return")
+
+                elif upgrade_input_low == "hp":
+                    character_creation.player.hp += 25
+                    character_creation.player.statpoints -= 1
+                    print("\nHP successfully upgraded!")
+                    input("\nPress Enter to return")
+
+                else:
+                    print("That is an invalid selection. Please type in your selection from the above fighter stats")
+
+                    user_input = 1
+
+                    input("Press Enter to try again")
+                    
+            elif user_input == 2:
+                character_creation.character_details()
+
+            elif user_input == 3:
+                break
 
             else:
-                print("That is an invalid selection. Please type in your selection from the above fighter stats")
-
-                user_input = 1
+                print("That's an invalid selection.")
 
                 input("Press Enter to try again")
-                
-        elif user_input == 2:
-            character_creation.character_details()
 
-        elif user_input == 3:
-            break
+        if character_creation.player.statpoints == 0:
+            os.system('clear')
+        
+            print("Welcome to the upgrade menu")
+            print("\nHere you will be able to spend available stat points to upgrade your fighter's stats")
+            print("\nYou currently do not have any statpoints available to spend")
 
-        else:
-            print("That's an invalid selection.")
+            input("\nPress Enter to return to previous menu")
+    except ValueError:
+        print("\nThat was an invalid selection")
+        print("Please enter a number between 1 and 3")
 
-            input("Press Enter to try again")
+        input("\nPress Enter to try again")
 
-    if character_creation.player.statpoints == 0:
-        os.system('clear')
-    
-        print("Welcome to the upgrade menu")
-        print("\nHere you will be able to spend available stat points to upgrade your fighter's stats")
-        print("\nYou currently do not have any statpoints available to spend")
+        upgrades()
 
-        input("\nPress Enter to return to previous menu")
-            
 
                              
 
