6458d39cb554a9ae1554fe8654a94d43c08cac88
shalulu94 <shahrulnasir290@hotmail.com>
2024-05-03 15:56:55 +1000

    Defining key features within README

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt install python3.10-venv
-python3 -m venv .venv
-source .venv/bin/activate
+# apt install python3.10-venv
+# python3 -m venv .venv
+# source .venv/bin/activate
 pip3 install -r requirements.txt
 python3 main.py
