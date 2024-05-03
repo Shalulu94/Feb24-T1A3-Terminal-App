41bfc67bfa7b3c902c7969c796b06e7b617fb85a
Shalulu94 <shahrulnasir290@hotmail.com>
2024-05-03 21:37:44 +1000

    Updated script with conditionals

--- run.sh
+++ run.sh
@@ -1,13 +1,52 @@
-#!/bin/bash
+#!/bin/bash -l
 
-# Added in this install function post testing. 
-# Terminal prompted that python3.10-venv needed to be installed prior to being able
-# to initialise a virtual environment
-apt install python3.10-venv 
+# Check if python is installed. If not, exit program
+# prompt user to install python
 
+if ! [[ -x "$(command -v python3)" ]]
+then
+  echo 'Error: 
+    You currently do not have python installed. Python is required to run this application.
+    To install Python, check out https://www.python.org/downloads/' >&2
+  exit 1
+fi
+
+python3 -m pip install --upgrade pip
+python3 -m pip --version
+
+
+
+# if ! [[ -x "$(command -v "]]
 # Original base script. activate a venv, install colored==2.2.4 as per requirements.txt
 # run main.py which is the application. 
+
+
+
+if [[ -x "$(command -v python install -r requirements.txt)" ]]
+then
+  $(command -v python install -r requirements.txt)
+  exit 1
+fi
+
+if [[ -x "$(command -v pip3 install -r requirements.txt)" ]]
+then
+  $(command -v pip3 install -r requirements.txt)
+  exit 1
+fi
+
+if [[ -x "$(command -v pip install -r requirements.txt)" ]]
+then
+  $(command -v pip install -r requirements.txt)
+  exit 1
+fi
+
+if [[ -x "$(command -v python3 install -r requirements.txt)" ]]
+then
+  $(command -v python3 install -r requirements.txt)
+  exit 1
+fi
+
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install -r requirements.txt
 python3 main.py
+
