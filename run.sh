d41f80c786a9c7b8930d0d754cc13420a9001e6e
shalulu94 <shahrulnasir290@hotmail.com>
2024-05-03 16:22:14 +1000

    Defining key features within README

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# apt install python3.10-venv
-# python3 -m venv .venv
-# source .venv/bin/activate
+# Added in this install function post testing. 
+# Terminal prompted that python3.10-venv needed to be installed prior to being able
+# to initialise a virtual environment
+apt install python3.10-venv 
+
+# Original base script. activate a venv, install colored==2.2.4 as per requirements.txt
+# run main.py which is the application. 
+python3 -m venv .venv
+source .venv/bin/activate
 pip3 install -r requirements.txt
 python3 main.py
